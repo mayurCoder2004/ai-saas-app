03998cf41c76124788bc39b396928f48c3b021b5
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2025-09-07 18:19:30 +0530

    Adding layout page of dashboard

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { useUser } from '@clerk/clerk-react'
+import React from 'react'
+
+const Sidebar = ({ sidebar, setSidebar }) => {
+    const {user} = useUser();
+  return (
+    <div className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:translate-x-full'} transition-all duration-300 ease-in-out`}>
+        <div className='my-7 w-full'>
+            <img src='' alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
