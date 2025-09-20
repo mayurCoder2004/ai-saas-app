6d655ba910ef961768c0feea7b8ecd594d093da2
mayurCoder2004 <mayursomnathpawar123@gmail.com>
2025-09-20 19:34:50 +0530

    Adding resume review functionality

--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,11 +1,40 @@
 import { FileText, Sparkles } from 'lucide-react';
 import React, { useState } from 'react'
+import axios from "axios";
+import { useAuth } from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const ReviewResume = () => {
   const [input, setInput] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+  const { getToken } = useAuth();
     
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append("resume", input);
+
+      const { data } = await axios.post(
+        "/api/ai/resume-review",
+        formData,
+        { headers: { Authorization: `Bearer ${await getToken()}` } }
+      );
+
+      if (data.success) {
+        setContent(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+    setLoading(false);
   };
   return (
     <div className="h-full overflow-y-scroll p-6 flex flex-col lg:flex-row items-start gap-6 text-slate-700">
@@ -35,7 +64,12 @@ const ReviewResume = () => {
 
         {/* Button */}
         <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#0066FF] via-[#FF8000] to-[#6A5ACD] text-white px-4 py-3 mt-8 text-sm font-medium rounded-lg cursor-pointer shadow-md hover:shadow-lg hover:opacity-95 transition">
-          <FileText className="w-5" /> Review Resume
+          {
+            loading ? <span className="w-4 h-4 my-1 rounded-full border-2 border-t-transparent animate-spin"></span>
+            :
+            <FileText className="w-5" />
+          }
+           Review Resume
         </button>
       </form>
 
